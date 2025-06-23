bc309f0b63300f454e6d3b05797b30573f837769
Ella Mae <ella@orange.me>
2025-06-23 11:12:16 -0400

    wip

--- src/gameboard.rs
+++ src/gameboard.rs
@@ -1,4 +1,4 @@
-use pelican_ui::events::{Event, OnEvent, TickEvent};
+use pelican_ui::events::{Event, Key, KeyboardEvent, KeyboardState, NamedKey, OnEvent, TickEvent};
 use pelican_ui::drawable::{Drawable, Component, Shape, ShapeType, Color};
 use pelican_ui::layout::{Area, SizeRequest, Layout};
 use pelican_ui::{Context, Component};
@@ -6,54 +6,81 @@ use pelican_ui::{Context, Component};
 use pelican_ui_std::{Stack, Offset, Size, Padding, OutlinedRectangle, Bin};
 
 use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::sync::{Arc, Mutex};
+use indexmap::IndexMap;
 
 use crate::Sprite;
 use crate::CollisionEvent;
 
-#[derive(Debug, Default, Serialize, Deserialize, Clone, Copy)]
-pub struct GameboardSize(pub f32, pub f32);
+#[derive(Debug, Default, Copy, Clone, Serialize, Deserialize)]
+pub enum AspectRatio {
+    OneOne,
+    TwoThree,
+    FourFive,
+    FiveSeven,
+    #[default]
+    SixteenNine,
+}
 
-#[derive(Debug, Component)]
-pub struct Gameboard(GameGrid, GameboardBackground, Vec<Box<dyn Sprite>>);
+impl AspectRatio {
+    pub fn size(&self, screen_size: (f32, f32)) -> (f32, f32) {
+        let (screen_w, screen_h) = screen_size;
+        let mut aspect = match self {
+            AspectRatio::OneOne => 1.0,
+            AspectRatio::TwoThree => 2.0 / 3.0,
+            AspectRatio::FourFive => 4.0 / 5.0,
+            AspectRatio::FiveSeven => 5.0 / 7.0,
+            AspectRatio::SixteenNine => 16.0 / 9.0,
+        };
+
+        match screen_w > screen_h {
+            true if screen_h < screen_w * aspect => (screen_h * aspect, screen_h),
+            false if screen_w < screen_h * aspect => (screen_w, screen_w * aspect),
+            true => (screen_w, screen_w * aspect),
+            false => (screen_h * aspect, screen_h),
+        }
+    }
+}
+
+type OnGameEvent = Box<dyn FnMut(&mut Gameboard, &mut Context, &mut dyn Event) -> bool>;
+
+#[derive(Component)]
+pub struct Gameboard(pub GameGrid, pub GameboardBackground, pub HashMap<String, Sprite>, #[skip] Option<OnGameEvent>);
 
 impl Gameboard {
-    pub fn new(ctx: &mut Context, aspect_ratio: AspectRatio) -> Self {
+    pub fn new(ctx: &mut Context, aspect_ratio: AspectRatio, on_event: OnGameEvent) -> Self {
         let colors = ctx.theme.colors;
         let background = GameboardBackground::new(ctx, 1.0, 8.0, colors.background.secondary, aspect_ratio);
-        Gameboard(GameGrid(vec![(Offset::Center, Offset::Center)], aspect_ratio), background, Vec::new())
+        Gameboard(GameGrid::new(HashMap::from([("background".to_string(), (Offset::Start, Offset::Start))]), aspect_ratio), background, HashMap::new(), Some(on_event))
     }
 
-    pub fn insert_sprite(&mut self, mut sprite: impl Sprite) {
-        let (ow, oh) = sprite.offset();
-        self.0.0.push((*ow, *oh));
-        self.2.push(Box::new(sprite));
+    pub fn insert_sprite(&mut self, ctx: &mut Context, sprite: Sprite) {
+        self.0.0.insert(sprite.id().to_string(), *sprite.offset());
+        self.2.insert(sprite.id().to_string(), sprite);
     }
 }
 
 impl OnEvent for Gameboard {
     fn on_event(&mut self, ctx: &mut Context, event: &mut dyn Event) -> bool {
-        if let Some(TickEvent) = event.downcast_ref::<TickEvent>() {
-            let GameboardSize(maxw, maxh) = ctx.state().get::<GameboardSize>();
-            let children = &self.2;
-
-            children.iter().enumerate().for_each(|(i, a)| {
-                let (ax, ay) = a.position((maxw, maxh));
-                let (aw, ah) = a.dimensions();
-
-                children.iter().skip(i + 1).for_each(|b| {
-                    let (bx, by) = a.position((maxw, maxh));
-                    let (bw, bh) = b.dimensions();
-
-                    if  ax < bx + bw && ax + aw > bx && ay < by + bh && ay + ah > by {
-                        ctx.trigger_event(CollisionEvent(a.id()));
-                        ctx.trigger_event(CollisionEvent(b.id()));
-                    }
-                });
-            });
-        }
-        true
+        let mut callback = self.3.take().expect("callback should be set");
+        let result = callback(self, ctx, event);
+        self.3 = Some(callback);
+        result
     }
 }
+
+impl std::fmt::Debug for Gameboard {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Gameboard")
+    }
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, Clone, Copy)]
+pub struct GameboardSize(pub f32, pub f32);
+
+impl GameboardSize{pub fn get(&self) -> (f32, f32) {(self.0, self.1)}}
 
 #[derive(Debug)]
 pub struct GameboardBackground(Shape, AspectRatio);
@@ -77,61 +104,29 @@ impl Component for GameboardBackground {
     fn build(&mut self, ctx: &mut Context, size: (f32, f32), _children: Vec<SizeRequest>) -> Vec<Area> {
         let new_size = self.1.size(size);
         if let ShapeType::RoundedRectangle(_, s, _) = &mut self.0.shape { *s = new_size; }
-        ctx.state().set(&GameboardSize(new_size.0, new_size.1));
+        ctx.state().set(GameboardSize(new_size.0, new_size.1));
         vec![Area { offset: (Offset::Start.get(size.0, new_size.0), Offset::Start.get(size.1, new_size.1)), size }]
     }
 }
 
-#[derive(Debug, Copy, Clone)]
-pub enum AspectRatio {
-    OneOne,
-    TwoThree,
-    FourFive,
-    FiveSeven,
-    SixteenNine,
-}
+#[derive(Debug, Default)]
+pub struct GameGrid(pub IndexMap<String, (Offset, Offset)>, AspectRatio);
 
+impl GameGrid {
+    pub fn new(offsets: HashMap<String, (Offset, Offset)>, ratio: AspectRatio) -> Self {
+        let mut new = IndexMap::new();
+        offsets.into_iter().for_each(|(k, v)| {new.insert(k, v);});
+        GameGrid(new, ratio)
+    }
 
-// impl AspectRatio {
-//     pub fn size(&self, size: (f32, f32)) -> (f32, f32) {
-//         let max = size.0.max(size.1);
-//         let min = size.0.min(size.1);
-//         match self {
-//             AspectRatio::OneOne => (min, min),
-//             AspectRatio::TwoThree => (size.0 > size.1).then(|| (max, max * 0.6667)).unwrap_or_else(|| (max * 0.6667, max)),
-//             AspectRatio::FourFive => (size.0 > size.1).then(|| (max, max * 0.8)).unwrap_or_else(|| (max * 0.8, max)),
-//             AspectRatio::FiveSeven => (size.0 > size.1).then(|| (max, max * 0.7143)).unwrap_or_else(|| (max * 0.7143, max)),
-//             AspectRatio::SixteenNine => (size.0 > size.1).then(|| (max, max * 0.5625)).unwrap_or_else(|| (max * 0.5625, max))
-//         }
-//     }
-// }
-
-impl AspectRatio {
-    pub fn size(&self, screen_size: (f32, f32)) -> (f32, f32) {
-        let (screen_w, screen_h) = screen_size;
-        let mut aspect = match self {
-            AspectRatio::OneOne => 1.0,
-            AspectRatio::TwoThree => 2.0 / 3.0,
-            AspectRatio::FourFive => 4.0 / 5.0,
-            AspectRatio::FiveSeven => 5.0 / 7.0,
-            AspectRatio::SixteenNine => 16.0 / 9.0,
-        };
-
-        match screen_w > screen_h {
-            true if screen_h < screen_w * aspect => (screen_h * aspect, screen_h),
-            false if screen_w < screen_h * aspect => (screen_w, screen_w * aspect),
-            true => (screen_w, screen_w * aspect),
-            false => (screen_h * aspect, screen_h),
-        }
+    pub fn size(&self, ctx: &mut Context) -> (f32, f32) {
+        ctx.state().get_or_default::<GameboardSize>().get()
     }
 }
 
-
-#[derive(Debug)]
-pub struct GameGrid(Vec<(Offset, Offset)>, AspectRatio);
-
 impl Layout for GameGrid {
-    fn request_size(&self, _ctx: &mut Context, children: Vec<SizeRequest>) -> SizeRequest {
+    fn request_size(&self, ctx: &mut Context, children: Vec<SizeRequest>) -> SizeRequest {
+        // *self.2.lock().unwrap() = ctx.state().get::<GameboardSize>().get();
         let (widths, heights): (Vec<_>, Vec<_>) = children.into_iter().map(|i|
             ((i.min_width(), i.max_width()), (i.min_height(), i.max_height()))
         ).unzip();
@@ -145,8 +140,9 @@ impl Layout for GameGrid {
     }
 
     fn build(&self, _ctx: &mut Context, max_size: (f32, f32), children: Vec<SizeRequest>) -> Vec<Area> {
+        // println!("Children {:?}", children);
         let new_size = self.1.size(max_size);
-        children.into_iter().zip(self.0.clone()).map(|(c, offset)| {
+        children.into_iter().zip(self.0.clone().values()).map(|(c, offset)| {
             let size = c.get(new_size);
             let x = offset.0.get(new_size.0, size.0);
             let y = offset.1.get(new_size.1, size.1);
--- src/sprite.rs
+++ src/sprite.rs
@@ -1,36 +1,33 @@
+use pelican_ui::events::{Event, OnEvent, TickEvent};
+use pelican_ui::drawable::{Drawable, Component, Shape, ShapeType, Color, Image};
+use pelican_ui::layout::{Area, SizeRequest, Layout};
 use pelican_ui::{Context, Component};
-use pelican_ui::drawable::{Drawable, Image, ShapeType};
-use pelican_ui_std::Offset;
 
-pub trait Sprite: Drawable {
-    fn dimensions(&self) -> (f32, f32);
-    fn position(&self, max_size: (f32, f32)) -> (f32, f32);
-    fn offset(&mut self) -> (&mut Offset, &mut Offset);
-    fn id(&self) -> u32;
-}
+use pelican_ui_std::{Stack, Offset, Size, Padding, OutlinedRectangle, Bin};
 
-pub struct SpriteImage;
+#[derive(Debug, Component)]
+pub struct Sprite(Stack, Image, #[skip] String, #[skip] (Offset, Offset), #[skip] (f32, f32));
+impl OnEvent for Sprite {}
 
-impl SpriteImage {
-    pub fn new(ctx: &mut Context, path: &str, size: (f32, f32)) -> Image {
+impl Sprite {
+    pub fn new(ctx: &mut Context, id: &str, path: &str, size: (f32, f32), offsets: (Offset, Offset)) -> Self {
         let bytes = ctx.assets.add_image(image::load_from_memory(&ctx.assets.load_file(path).unwrap()).unwrap().into());
         let image = Image{shape: ShapeType::Rectangle(0.0, (size.0, size.1)), image: bytes, color: None}; 
-        image
+        Sprite(Stack::default(), image, id.to_string(), offsets, (0.0, 0.0))
     }
+
+    pub fn dimensions(&mut self) -> &mut (f32, f32) { 
+        match &mut self.1.shape {
+            ShapeType::Ellipse(_, size) => size,
+            ShapeType::Rectangle(_, size) => size,
+            ShapeType::RoundedRectangle(_, size, _) => size,
+        }
+    }
+
+    // fn offset(&mut self) -> (&mut Offset, &mut Offset) {(&mut self.2.0, &mut self.2.1)}
+    pub fn id(&self) -> &String { &self.2 }
+
+    pub fn offset(&self) -> &(Offset, Offset) { &self.3 }
+
+    pub fn position(&mut self) -> &mut (f32, f32) { &mut self.4 }
 }
-
-// pub fn size(&self, ctx: &mut Context) -> (f32, f32) {
-//     let sw = self.size_request(ctx);
-//     (sw.max_width(), sw.max_height())
-// }
-
-// pub fn position(&self, max_size: (f32, f32)) -> (f32, f32) {
-//     let (minw, minh, maxw, maxh) = self.request_size(ctx);
-//     let x = self.0.0.get(max_size.0, maxw);
-//     let y = self.0.1.get(max_size.1, maxh);
-//     (x, y)
-// }
-
-// pub fn offset(&self) -> (&mut Offset, &mut Offset) {
-//     (&mut self.0.0, &mut self.0.1)
-// }
